87a55a8a099d33801b4b9b687f78dfbac4b61980
Sophie <sophiexhuang@gmail.com>
2016-06-08 17:34:24 +0000

    workshop part V1

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,21 +1,26 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
 var circleRadius;
+
 var rVal;
 var gVal;
 var bVal;
 
 function setup() {
     createCanvas(480, 600);
+    
+    frameRate(5);
+    
     circleDiameter = width/NUM_CIRCLES;
     circleRadius = circleDiameter/2;
-}
-
-function draw() {
+    
     rVal = 255;
     gVal = 0;
     bVal = 0;
-    
+}
+
+function draw() {
     var isShifted = false;
     
     var y = height;
@@ -30,8 +35,8 @@ function draw() {
         }
         
         while (x <= width) {
-            fill(color(rVal, gVal, bVal));
             stroke(color(rVal, gVal, bVal));
+            fill(color(rVal, gVal, bVal));
             ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
         }
@@ -39,14 +44,14 @@ function draw() {
         y = y - circleRadius;
         isShifted = !isShifted;
         
-        rVal = rVal - 2;
-        gVal = gVal + 7;
-        bVal = bVal + 3;
+        rVal = (rVal + 254) % 256;
+        gVal = (gVal + 7) % 256;
+        bVal = (bVal + 3) % 256;
     }
 }
 
 function keyPressed() {
-    if (keyCode === 115 || keyCode = 83) {
+    if (keyCode === 115 || keyCode === 83) {
         saveCanvas('geometricPattern', 'png');
     }
     return false;
