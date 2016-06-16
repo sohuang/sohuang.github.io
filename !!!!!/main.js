42b7e797719021c8d205972314c6128dbeda4430
Sophie <sophiexhuang@gmail.com>
2016-06-16 22:50:57 +0000

    freeform project

---
+++ !!!!!/main.js
@@ -0,0 +1,71 @@
+function veryFirstFunction() {
+     document.getElementById("veryFirstThings").style.display = "none";
+     document.getElementById("firstThings").style.display = "block";
+}
+
+function muchoFirstFunction() {
+     document.getElementById("veryFirstThings").style.display = "none";
+     document.getElementById("unoThings").style.display = "block";
+}
+
+function firstFunction() {
+     document.getElementById("unoThings").style.display = "none";
+     document.getElementById("firstThings").style.display = "none";
+     document.getElementById("secondThings").style.display = "block";
+}
+
+function unoFunction() {
+     document.getElementById("unoThings").style.display = "none";
+     document.getElementById("firstThings").style.display = "none";
+     document.getElementById("dosThings").style.display = "block";
+}
+
+function secondFunction() {
+     document.getElementById("dosThings").style.display = "none";
+     document.getElementById("secondThings").style.display = "none";
+     document.getElementById("allTitles").style.display = "none";
+     document.getElementById("returnButton").style.display = "block";
+     document.getElementById("returnButton").style.display = "block";
+}
+
+function twoOneFunction() {
+     document.getElementById("fddkdf").style.display = "block";
+     secondFunction();
+}
+function twoTwoFunction() {
+     document.getElementById("fddkdf").style.display = "block";
+     secondFunction();
+}
+function twoThreeFunction() {
+     document.getElementById("fddkdf").style.display = "block";
+     secondFunction();
+}
+function twoFourFunction() {
+     document.getElementById("fddkdf").style.display = "block";
+     secondFunction();
+}
+
+function dosOneFunction() {
+     document.getElementById("fddkdf").style.display = "block";
+     secondFunction();
+}
+function dosTwoFunction() {
+     document.getElementById("fddkdf").style.display = "block";
+     secondFunction();
+}
+function dosThreeFunction() {
+     document.getElementById("fddkdf").style.display = "block";
+     secondFunction();
+}
+function dosFourFunction() {
+     document.getElementById("fddkdf").style.display = "block"
+     secondFunction();
+}
+
+function returnFunction() {
+     document.getElementById("fddkdf").style.display = "none";
+     document.getElementById("veryFirstThings").style.display = "block";
+     document.getElementById("allTitles").style.display = "block";
+     document.getElementById("returnButton").style.display = "none";
+}
+
---
+++ frfopr/main.js
@@ -0,0 +1,21 @@
+
+
+function setup() {
+     var canvas = createCanvas(155, 155);
+}
+
+function draw() {
+     background(0);
+     // var oneOne = 
+     var oneOne = createShape(RECT, 25, 25, 50, 50);
+     // oneOne.parent("firstThings");
+     // var oneTwo = 
+     rect(25, 80, 50, 50);
+     // oneTwo.parent("firstThings");
+     // var oneThree = 
+     rect(80, 25, 50, 50);
+     // oneThree.parent("firstThings");
+     // var oneFour = 
+     rect(80, 80, 50, 50);
+     // oneFour.parent("firstThings");
+}
