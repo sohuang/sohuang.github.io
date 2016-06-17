f7e3dce927dd90d7b3901223944de4e604cb403f
Sophie <sophiexhuang@gmail.com>
2016-06-17 23:49:26 +0000

    ???

--- !!!!!/main.js
+++ !!!!!/main.js
@@ -1,11 +1,13 @@
 function veryFirstFunction() {
      document.getElementById("veryFirstThings").style.display = "none";
      document.getElementById("firstThings").style.display = "block";
+     document.getElementById("secretDiv").style.display = "none";
 }
 
 function muchoFirstFunction() {
      document.getElementById("veryFirstThings").style.display = "none";
      document.getElementById("unoThings").style.display = "block";
+     document.getElementById("secretDiv").style.display = "none";
 }
 
 function firstFunction() {
@@ -75,5 +77,29 @@ function returnFunction() {
      document.getElementById("veryFirstThings").style.display = "block";
      document.getElementById("allTitles").style.display = "block";
      document.getElementById("returnButton").style.display = "none";
+     document.getElementById("secretDiv").style.display = "block";
 }
 
+function secretFunction() {
+     var myRandom = Math.round(Math.random() * 9);
+     var secretUrl = new Array();
+     secretUrl[0] = "http://weavesilk.com/";
+     secretUrl[1] = "http://eelslap.com/";
+     secretUrl[2] = "http://29a.ch/sandbox/2011/neonflames/";
+     secretUrl[3] = "http://sorrysloth.com/";
+     secretUrl[4] = "http://cat-bounce.com/";
+     secretUrl[5] = "http://www.zefrank.com/snm/index.html";
+     secretUrl[6] = "http://www.staggeringbeauty.com/";
+     secretUrl[7] = "http://www.unwrong.com/projects/fractalexplorer/";
+     secretUrl[8] = "http://slither.io/";
+     secretUrl[9] = "http://www.barcinski-jeanjean.com/entries/line3d/";
+     secretUrl[10] = "http://maninthedark.com/";
+     secretUrl[11] = "http://realitytourist.net/inkblobs/";
+     secretUrl[12] = "http://drawminos.com/";
+     secretUrl[13] = "http://www.pointerpointer.com/";
+     secretUrl[14] = "http://www.koalastothemax.com/";
+     secretUrl[15] = "http://www.intotime.com/";
+     secretUrl[16] = "http://www.kanyezone.com/";
+     secretUrl[17] = "http://radiooooo.com/";
+     window.open(secretUrl[myRandom]);
+}
