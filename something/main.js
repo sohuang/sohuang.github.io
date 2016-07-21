6023486110e2a64496f823614dfc59c425a80228
Sophie <sophiexhuang@gmail.com>
2016-07-21 16:44:11 +0000

    leaflet shows up

--- something/main.js
+++ something/main.js
@@ -1,8 +1,8 @@
-var mymap = L.map('mapid').setView([-122.425000, 37.774], 12);
+var mymap = L.map('mapid').setView([37.779116, -122.415766], 12);
 
-L.tileLayer('', {
+L.tileLayer('https://api.mapbox.com/styles/v1/sohuang/ciqvvfyla000objm7xf7bxjet/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic29odWFuZyIsImEiOiJjaXF2dmYzbHIwMDJ2ZnBtZzc3MWhzaXMwIn0.ujgNWplIcJ4agN7XxexrhA', {
      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
      maxZoom: 18,
-     id: 'sohuang.0nffe7lg',
-     accessToken: 'pk.eyJ1Ijoic29odWFuZyIsImEiOiJjaXF2dmYzbHIwMDJ2ZnBtZzc3MWhzaXMwIn0.ujgNWplIcJ4agN7XxexrhA'
+     //id: 'sohuang.0nffe7lg',
+     //accessToken: 'pk.eyJ1Ijoic29odWFuZyIsImEiOiJjaXF2dmYzbHIwMDJ2ZnBtZzc3MWhzaXMwIn0.ujgNWplIcJ4agN7XxexrhA'
 }).addTo(mymap);
