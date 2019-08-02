401592f7abe859fa3d8e2f44c36802e9eb71f3f8
sohuang <sophiexhuang@gmail.com>
2019-08-02 13:08:43 -0700

    background

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // idk but changing bg doesn't work if this file is not in same dir as index
 
 function init() {
-	document.body.style.backgroundImage = "url(home/bg" + "0" + (Math.floor(Math.random() * 6) + 1) + ".png)";
+	var bg = Math.floor(Math.random() * 14) + 1;
+	var ext = "png";
+	if (bg >= 8)
+	{
+		ext = "gif";
+	}
+	document.body.style.backgroundImage = "url(home/bg" + bg + "." + ext + ")";
+
 	document.body.style.color = "#FFFFFF";
 }
 window.onload = init;
